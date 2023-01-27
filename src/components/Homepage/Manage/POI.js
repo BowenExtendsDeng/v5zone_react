4588e152cebc30fcd4986aa7b523e76a9fe0726c
bowen <2631702650@qq.com>
2023-01-28 00:50:06 +0800

    写完了导出Excel和Zip的功能

--- src/components/Homepage/Manage/POI.js
+++ src/components/Homepage/Manage/POI.js
@@ -1,7 +1,31 @@
 import {Button, Stack, Typography} from "@mui/material";
 import React from "react";
+import axios from "axios";
 
 export default function POI(){
+
+    const openInNewTab = url => {
+        // 👇️ setting target to _blank with window.open
+        window.open(url, '_blank', 'noopener,noreferrer');
+    };
+    function handleMembersDownload() {
+        const url = axios.defaults.baseURL
+            + "/poi/members/members.xlsx"
+        openInNewTab(url);
+    }
+
+    function handleBillsDownload() {
+        const url = axios.defaults.baseURL
+            + "/poi/bills/bills.xlsx"
+        openInNewTab(url);
+    }
+
+    const handleInvoiceDownload = (event) => {
+        const url = axios.defaults.baseURL
+            + "/poi/invoice/" + event.target.value + ".zip"
+        openInNewTab(url);
+    }
+
     return(
         <Stack>
             <Typography
@@ -22,9 +46,7 @@ export default function POI(){
                     fontWeight: "bold",
                 }}
                 variant="outlined"
-                onClick={() => {
-                    alert('bye')
-                }}>
+                onClick={handleMembersDownload}>
                 导出队员信息excel
             </Button>
             <Typography
@@ -45,10 +67,42 @@ export default function POI(){
                     fontWeight: "bold",
                 }}
                 variant="outlined"
-                onClick={() => {
-                    alert('bye')
-                }}>
+                onClick={handleBillsDownload}>
                 导出V5账单
+            </Button>
+            <Typography
+                sx={{
+                    margin: 3,
+                    fontFamily: "黑体",
+                    fontWeight: "bold",
+                    fontSize: 20,
+                }}
+            >
+                V5发票
+            </Typography>
+            <Button
+                size="large"
+                sx={{
+                    margin:2,
+                    textAlign: "center",
+                    fontWeight: "bold",
+                }}
+                variant="outlined"
+                value={new Date().getFullYear()}
+                onClick={handleInvoiceDownload}>
+                导出{new Date().getFullYear()}年发票
+            </Button>
+            <Button
+                size="large"
+                sx={{
+                    margin:2,
+                    textAlign: "center",
+                    fontWeight: "bold",
+                }}
+                variant="outlined"
+                value={"all"}
+                onClick={handleInvoiceDownload}>
+                导出全部发票
             </Button>
         </Stack>
     )
